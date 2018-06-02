bf82305fa8e18910f11d35c560da33b77f67582e
Srdjan <srdjanprokin@sbb.rs>
2018-06-02 15:05:14 +0200

    dodali smo bazu za heroku

--- config/database.js
+++ config/database.js
@@ -1,5 +1,8 @@
 module.exports = {
-    database: "mongodb://localhost:27017/salestrekkerappBaza",
+    // database: "mongodb://localhost:27017/salestrekkerappBaza",
+    // ovo je za heroku database
+    database:'mongodb://srdjan:Misolovka1@ds245680.mlab.com:45680/salestrekkerappbaza',
+
     // ovo je za token secret
     secret:"mojatajna"
 }
