73795b0ae42b3c8f12093e304d724073367eb1b3
Srdjan <srdjanprokin@sbb.rs>
2018-06-03 06:22:02 +0200

    popravio sam route kad je error

--- app.js
+++ app.js
@@ -50,9 +50,9 @@ app.use("/users", users);
 app.use("/form", formData);
 
 // ovo smo napisali da nebi imali error na startu
-app.get('/',(req, res) => {
-    res.send('invalid endpoint');
-});
+// app.get('/',(req, res) => {
+//     res.send('invalid endpoint');
+// });
 
 // app.use(function(req, res, next) {
 //     res.header("Access-Control-Allow-Origin", "*");
@@ -64,6 +64,11 @@ app.get('/',(req, res) => {
 // app.get('/form/finalForm/:id', function (req, res, next) {
 //     res.json({msg: 'This is CORS-enabled for all origins!'})    
 // });
+app.use((error, req, res, next)=>{
+    res.status(error.status || 500);
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
 
 app.get('*', (req, res) => {
     // res.redirect('/');
